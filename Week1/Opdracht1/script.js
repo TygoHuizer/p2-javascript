bac68d4b1ff1c1d44cb25a0b09cb5b172d5301a5
TygoHuizer <9026105@student.zadkine.nl>
2025-11-25 11:43:50 +0100

    Add basic HTML and JS for Week1 Opdracht1
    Introduces index.html with a header and script.js that prompts for a name and updates the header text and color. Sets up the initial structure for the assignment.

---
+++ Week1/Opdracht1/script.js
@@ -0,0 +1,7 @@
+        let naam = prompt("Wat is je naam?");
+
+
+
+        document.getElementById("titel").style.color = "red";
+        document.getElementById("titel").innerHTML = "doei " + naam;
+
