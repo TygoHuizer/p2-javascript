74186d7d2bc9bb1ec8d4e0b11b07d362e109ed7a
TygoHuizer <9026105@student.zadkine.nl>
2025-11-27 13:20:13 +0100

    Add Opdracht3 JavaScript prompt and update HTML
    Introduced Main.js in Opdracht3 to prompt for user's name and display a welcome message. Updated index.html to include the new script and an element for output. Removed Styles.css and added Bonus.png. Minor formatting improvements in Opdracht1 index.html.

---
+++ Week1/Opdracht3/Main.js
@@ -0,0 +1,4 @@
+let wat = prompt("Wat is uw naam?");
+
+document.getElementById("wat").innerHTML = "Hallo " + wat + " welkom bij mijn eerste JavaScript-pagina!";
+console.log("Hallo " + wat + " welkom bij mijn eerste JavaScript-pagina!");
