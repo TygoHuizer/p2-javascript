4547cb515669204619d6011b1b2e5697c89f2762
TygoHuizer <9026105@student.zadkine.nl>
2025-12-02 11:26:33 +0100

    Add show/hide paragraph functionality
    Introduces index.html and script.js for toggling the visibility of a paragraph using buttons. The 'Toon tekst' button also sets the background color to LightBlue.

---
+++ Week2/Opdracht9/script.js
@@ -0,0 +1,7 @@
+function verberg_tekst() { 
+    document.getElementById("EenId").style.display = "none";
+}
+function toon_tekst() { 
+    document.getElementById("EenId").style.display = "block";
+    document.getElementById("EenId").style.backgroundColor = "LightBlue";
+}
