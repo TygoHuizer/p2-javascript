82cecfddfa94b209e832d9a0c028be34b2bb4a56
TygoHuizer <9026105@student.zadkine.nl>
2025-12-02 11:49:52 +0100

    Add greeting buttons and improve formatting
    Added Opdracht10 with HTML and JS for greeting buttons that change text and background color. Also improved formatting in Opdracht9 index.html for consistency.

---
+++ Week2/Opdracht10/script.js
@@ -0,0 +1,15 @@
+function groet_ochtend() {
+  document.getElementById("groet").innerHTML = "Goedemorgen!";
+  document.body.style.backgroundColor = "LightYellow";
+  document.body.style.color = "black";
+}
+function groet_middag() {
+  document.getElementById("groet").innerHTML = "Goedemiddag!";
+  document.body.style.backgroundColor = "Orange";
+  document.body.style.color = "black";
+}
+function groet_avond() {
+  document.getElementById("groet").innerHTML = "Goedenavond!";
+  document.body.style.backgroundColor = "DarkBlue";
+  document.body.style.color = "white";
+}
