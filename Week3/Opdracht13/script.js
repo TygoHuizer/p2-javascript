244750e40defbb417d2d8cab6241bb3849471e02
TygoHuizer <9026105@student.zadkine.nl>
2025-12-08 09:14:17 +0100

    fix opdracht 13

--- Week3/Opdracht13/script.js
+++ Week3/Opdracht13/script.js
@@ -2,16 +2,14 @@ let leef = prompt("Wat is je leeftijd?");
 if (leef < 18) {
   document.getElementById("leef").innerHTML =
     "Je mag hier nog niet naar binnen.";
-      document.getElementById("leef").style.color = "red";
-} 
+  document.getElementById("leef").style.color = "red";
+}
 if (leef >= 18) {
-      document.getElementById("leef").style.color = "green";
+  document.getElementById("leef").style.color = "green";
 }
-if (leef >= 18 && leef <= 65) { 
-      document.getElementById("leef").innerHTML =
-    "Welkom, volwassen bezoeker!";
+if (leef >= 18 && leef <= 65) {
+  document.getElementById("leef").innerHTML = "Welkom, volwassen bezoeker!";
 }
-if (leef => 65) {
-  document.getElementById("leef").innerHTML =
-    "Welkom, senior bezoeker!";
+if ((leef) >= 65) {
+  document.getElementById("leef").innerHTML = "Welkom, senior bezoeker!";
 }
