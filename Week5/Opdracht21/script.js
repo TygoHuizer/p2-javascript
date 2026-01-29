9843951309dc2188f1a71010315ece2095087ad5
TygoHuizer <9026105@student.zadkine.nl>
2026-01-29 23:27:14 +0100

    Add Opdracht21 and Opdracht21.2 initial files
    Added initial HTML and JavaScript files for Opdracht21 and Opdracht21.2 in Week5. Opdracht21 includes a button and advice display, with script initializing activity and object arrays and DOM references. Opdracht21.2 includes basic HTML structure and an empty script file.

---
+++ Week5/Opdracht21/script.js
@@ -0,0 +1,5 @@
+let activiteit = ['een dutje doen', 'mediteren', 'wandelen'];
+let voorwerp = ['een kop thee', 'favoriets boek', 'telefoon uitzetten'];
+let button = document.getElementById("knop");
+let advies = document.getElementById("advies");
+
